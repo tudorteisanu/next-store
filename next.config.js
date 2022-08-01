160e23abc11b95e1324565af891e9e43219f2dbd
Tudor Teisanu <tudor.teisanu@powerit.dev>
2022-08-01 17:06:30 +0300

    fixed .env for axios

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone',
+    reactStrictMode: true,
+    swcMinify: true,
+    output: 'standalone',
+    env: {
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    }
 }
 
 module.exports = nextConfig
